c60db0b90adb8c13a5dcba33d5754c6e70cf31e3
Magaram Hidirnabiev <kurah8626@gmail.com>
2026-07-04 19:47:27 +0300

    Created the 'homework-7.js' file and practiced creating variables and functions.

---
+++ homework-7.js
@@ -0,0 +1,52 @@
+const checkWeather = (city, temperature) => {
+  console.log(`Сейчас в ${city} температура + ${temperature} градусов по Цельсию`);
+};
+
+checkWeather("Казань", 25); 
+
+
+const SPEED_OF_LIGHT = 299792458;
+
+const checkSpeed = (speed) => {
+  if (speed > SPEED_OF_LIGHT) {
+    console.log("Сверхсветовая скорость");
+  } else if (speed < SPEED_OF_LIGHT) {
+    console.log("Субсветовая скорость");
+  } else {
+    console.log("Скорость света");
+  }
+};
+
+checkSpeed(100000);
+checkSpeed(300000000);
+checkSpeed(299792458);
+
+
+const productName = "Смартфон";
+const productPrice = 500;
+
+const buyProduct = (budget) => {
+  if (budget >= productPrice) {
+    console.log(`${productName} приобретён. Спасибо за покупку!`);
+  } else {
+    const missingAmount = productPrice - budget;
+    console.log(`Вам не хватает ${missingAmount}$, пополните баланс`);
+  }
+};
+
+buyProduct(600);
+buyProduct(350);
+
+
+
+const calculateBMI = (weight, height) => {
+  const bmi = weight / (height * height);
+  console.log(`Ваш индекс массы тела: ${bmi.toFixed(2)}`);
+};
+
+
+const user = {
+  name: "Алексей",
+  isLoggedIn: true,
+  maxLoginAttempts: 3
+};
