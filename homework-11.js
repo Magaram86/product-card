3236a8da7ae07e022e0eef20bada6cfd66d026f9
Magaram Hidirnabiev <kurah8626@gmail.com>
2026-08-11 22:18:54 +0300

    Fixed the issue raised.

--- homework-11.js
+++ homework-11.js
@@ -1,29 +1,25 @@
-
 const openModalBtn = document.getElementById('open-modal-btn');
 const modalOverlay = document.getElementById('modal');
 const closeModalBtn = document.getElementById('close-modal-btn');
-
 const regForm = document.getElementById('reg-form');
 
 let user;
-
-openModalBtn.addEventListener('click', function () {
-
+openModalBtn.addEventListener('click', () => {
   modalOverlay.classList.add('modal-showed');
 });
 
-closeModalBtn.addEventListener('click', function () {
+closeModalBtn.addEventListener('click', () => {
   modalOverlay.classList.remove('modal-showed');
 });
 
-modalOverlay.addEventListener('click', function (event) {
+modalOverlay.addEventListener('click', (event) => {
   if (event.target === modalOverlay) {
     modalOverlay.classList.remove('modal-showed');
   }
 });
 
 if (regForm) {
-  regForm.addEventListener('submit', function (event) {
+  regForm.addEventListener('submit', (event) => {
     event.preventDefault();
 
     const passwordInput = document.getElementById('reg-password');
@@ -43,11 +39,8 @@ if (regForm) {
     };
 
     user = userData;
-
     console.log('Пользователь успешно зарегистрирован:', user);
-
     regForm.reset();
-
     modalOverlay.classList.remove('modal-showed');
   });
 }
